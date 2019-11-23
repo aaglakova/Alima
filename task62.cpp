562d0b002240adb206f76df7db8cc5040349500b
Alima <57910466+aaglakova@users.noreply.github.com>
2019-11-23 19:00:31 +0600

    Create task62.cpp

---
+++ task62.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string no;
+    cin>>no;
+    for(int i=0; i<5; i++){
+        if(no[i]%2==0){
+            no[i]='0';
+        }}
+    cout<<no;
+}
